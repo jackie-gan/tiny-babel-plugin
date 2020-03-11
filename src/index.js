09012e0678b07095c8f43e5fc455990c224a93a6
jackie-gan <jackiegan405679096@gmail.com>
2020-03-11 12:10:52 +0800

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
-import * as t from '@babel/types';
+/**
+ * 替换字符串 
+ */
+function transformOptionValue(value) {
+  return value.replace(/\$\$$/, '');
+}
 
-export default function() {
+/**
+ * 递归构造新节点
+ */
+function recursiveLogicalExpression(types, options, members) {
+  const node = members.pop();
+  if (members.length >= 1) {
+    if (options.indexOf(node) > -1) {
+      return types.logicalExpression('&&', node, recursiveLogicalExpression(types, options, members));
+    } else {
+      return recursiveLogicalExpression(types, options, members);
+    }
+  } else {
+    return node;
+  }
+}
+
+export default function({ types }) {
   return {
-    name: 'tiny-babel-plugins',
+    name: 'babel-plugin-tiny--optional-chaining',
     visitor: {
-      MemberExpression(path) {
-        console.log('MemberExpression');
+      MemberExpression: {
+        enter(path) {
+          if (types.isIfStatement(path.parent)) {
+            let currentNode = path.node;
+            const members = [];
+            const options = [];
+
+            // 先将所有的 $$ 转换好
+            while(types.isMemberExpression(currentNode)) {
+              if (types.isLiteral(currentNode.property)) {
+                const literalNode = currentNode.property;
+                if (literalNode.value.endsWith('$$')) {
+                  const value = transformOptionValue(literalNode.value);
+                  literalNode.value = value;
+                  literalNode.raw = value;
+                  options.push(currentNode);
+                }
+              } else if (types.isIdentifier(currentNode.property)) {
+                const identifierNode = currentNode.property;
+                if (identifierNode.name.endsWith('$$')) {
+                  const name = transformOptionValue(identifierNode.name);
+                  identifierNode.name = name;
+                  options.push(currentNode);
+                }
+              }
+
+              members.push(currentNode);
+
+              currentNode = currentNode.object;
+            }
+
+            if (types.isIdentifier(currentNode) && currentNode.name.endsWith('$$')) {
+              const name = transformOptionValue(currentNode.name);
+              currentNode.name = name;
+
+              options.push(currentNode);
+            }
+
+            members.push(currentNode);
+
+            // 存在 optional chaining
+            if (options.length > 0) {
+              const root = recursiveLogicalExpression(types, options, members);
+
+              path.replaceWith(root);
+            }
+          }
+        }
       }
     }
   }
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,37 @@
 import * as babel from '@babel/core';
 import myPlugin from '../src/index';
 
-test('test', () => {
-  const code = 'const test = a.b.c.d';
-  const result = babel.transform(code, {
-    plugins: [
-      myPlugin
-    ]
-  })
+describe('plugin test', () => {
+  it('test case 1', () => {
+    const code = 'const test = a.b.c.d';
+    const result = babel.transform(code, {
+      plugins: [
+        myPlugin
+      ]
+    });
+
+    expect(result.code).toBe('const test = a.b.c.d;');
+  });
+
+  it('test case 2', () => {
+    const code = 'if (a$$.b$$.c.d$$.e) {}';
+    const result = babel.transform(code, {
+      plugins: [
+        myPlugin
+      ]
+    });
+
+    expect(result.code).toBe('if (a && a.b && a.b.c.d && a.b.c.d.e) {}');
+  });
+
+  it('test case 3', () => {
+    const code = 'if (a.b.c.d.e) {}';
+    const result = babel.transform(code, {
+      plugins: [
+        myPlugin
+      ]
+    });
+
+    expect(result.code).toBe('if (a.b.c.d.e) {}');
+  });
 });
